f774d031f80d44af6286a42e78a21d4cfca91e56
BALUNSZ\george.chiu <george.chiu@baluntek.com>
2018-12-28 15:45:03 +0800

    First Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2018年12月27日
+
+@author: George Chiu
+'''
+
+
+from robot import Voting_Robot
+
+if __name__ == '__main__':
+    robot = Voting_Robot()
+    robot.start()
