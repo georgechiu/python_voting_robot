156954634f4e1ee73333c4a180200a7c9cc841d7
BALUNSZ\george.chiu <george.chiu@baluntek.com>
2019-02-25 16:08:26 +0800

    Finish yidian robot

--- robot_yidian.py
+++ robot_yidian.py
@@ -8,10 +8,12 @@ Created on 2019年2月25日
 '''
 
 
+from io import BytesIO
 import random
+from PIL import Image
+from pytesseract import image_to_string
 from splinter import Browser
 from selenium.common.exceptions import NoAlertPresentException, TimeoutException, UnexpectedAlertPresentException
-from selenium.webdriver.support.ui import WebDriverWait
 from time import sleep
 
 
@@ -68,13 +70,90 @@ class Voting_Robot_Yidian(object):
         self.browser.find_by_xpath("//input[contains(@class, 'fbi_input') and contains(@class, 'aria-content')]").fill(self.random_mobile_number())
 
         self.browser.click_link_by_id("form_submit")
-        sleep(random.randint(1, 2))
+        while self.browser.is_element_not_present_by_xpath("//img[@class='cpt_img']"):
+            sleep(random.randint(1, 2))
 
-        self.browser.find_by_xpath("//input[contains(@class, 'fbi_input') and contains(@class, 'cpt_input')]").fill("ABCD")
+        while True:
+            text = self.get_captcha()
+            if text.isalnum():
+                self.browser.find_by_xpath("//input[contains(@class, 'fbi_input') and contains(@class, 'cpt_input')]").fill(text)
+                if self.browser.find_by_xpath("//i[contains(@class, 'iconfont') and contains(@class, 'cpt_icon') and contains(@class, 'succeed')]"):
+                    break
+            self.browser.driver.find_element_by_class_name("cpt_img").click()
+            sleep(random.random())
+        
+        self.browser.find_by_xpath("//a[contains(@class, 'fbc_button') and contains(@class, 'cpt_confirm')]").click()
 
-        sleep(60)
+        sleep(random.randint(1, 5))
     
     
     def random_mobile_number(self):
         prelist=["130","131","132","133","134","135","136","137","138","139","147","150","151","152","153","155","156","157","158","159","186","187","188"]
         return random.choice(prelist)+"".join(random.choice("0123456789") for i in range(8))
+
+    
+    def get_captcha(self):
+        screenshot = Image.open(BytesIO(self.browser.driver.get_screenshot_as_png()))
+
+        element = self.browser.driver.find_element_by_class_name("cpt_img")
+        location = element.location
+        size = element.size
+        left = location["x"]
+        top = location["y"]
+        right = left + size["width"]
+        bottom = top + size["height"]
+        area = (left, top, right, bottom)
+
+        captcha_img = screenshot.crop(area)
+        text = image_to_string(captcha_img)
+
+        return text
+
+
+    def binary_image(self, image, threshold):
+        gray_image = image.convert("L")
+        table = []
+
+        for i in range(256):
+            if i < threshold:
+                table.append(0)
+            else:
+                table.append(1)
+    
+        bin_image = gray_image.point(table, "1")
+        return bin_image
+
+
+    def clear_noise(self, image):
+        data = image.getdata()
+        w, h = image.size
+        black_point = 0
+
+        for x in range(1, w - 1):
+            for y in range(1, h - 1):
+                mid_pixel = data[w * y + x]
+                if mid_pixel < 50:
+                    top_pixel = data[w * (y - 1) + x]
+                    left_pixel = data[w * y + (x - 1)]
+                    down_pixel = data[w * (y + 1) + x]
+                    right_pixel = data[w * y + (x + 1)]
+                
+                    if top_pixel < 10:
+                        black_point += 1
+                    if left_pixel < 10:
+                        black_point += 1
+                    if down_pixel < 10:
+                        black_point += 1
+                    if right_pixel < 10:
+                        black_point += 1
+                    if black_point < 1:
+                        image.putpixel((x, y), 255)
+                
+                    black_point = 0
+
+        for x in range(1, w - 1):
+            for y in range(1, h - 1):
+                if x < 2 or y < 2:
+                    image.putpixel((x - 1, y - 1), 255)
+                if x > w - 3 or y > h - 3:
+                    image.putpixel((x + 1, y + 1), 255)
